d99e1fedd1103695bad1892f36898effccea24b5
marcel <marcel.maciaszek@icloud.com>
2023-04-15 17:14:17 +0200

    Initial Commit

---
+++ Recipe List App/Views/SecondRecipeList.swift
@@ -0,0 +1,8 @@
+//
+//  SecondRecipeList.swift
+//  Recipe List App
+//
+//  Created by Marcel Maciaszek on 10/04/2023.
+//
+
+import Foundation
---
+++ Recipe List App/Views/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  Recipe List App
+//
+//  Created by Marcel Maciaszek on 10/04/2023.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
