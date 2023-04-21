3e347936244c57f54b19b4c2cf4bc399b4055142
marcel <marcel.maciaszek@icloud.com>
2023-04-21 10:37:10 +0200

    Displaying Ingredient Portions

--- Recipe List App/ViewModels/RecipeModel.swift
+++ Recipe List App/ViewModels/RecipeModel.swift
@@ -15,13 +15,66 @@ class RecipeModel: ObservableObject {
     
     static func getPortion(ingredient:Ingredient, recipeServings:Int, targetServings:Int) -> String {
         
-        //get a single service size by multypling denominator by the recipe serbings
+        var portion = ""
+        var numerator = ingredient.num ?? 1
+        var denominator = ingredient.denom ?? 1
+        var wholePortions = 0
         
-        //get targett portion by multypling numerator by target servings
+        if ingredient.num != nil {
+            //get a single service size by multypling denominator by the recipe serbings
+            denominator *= recipeServings
+            
+            //get targett portion by multypling numerator by target servings
+            numerator *= targetServings
+            
+            //reduce fraction by greatest common divisor
+            let divisor = Rational.greatestCommonDivisor(numerator, denominator)
+            numerator /= divisor
+            denominator /= divisor
+            
+            //get the whole portion if numerator > denominator
+            if numerator >= denominator{
+                
+                // calc whole portions
+                wholePortions = numerator / denominator
+                
+                //calc the remainder
+                numerator = numerator % denominator
+                
+                // Assign to portion string
+                portion += String(wholePortions)
+            }
+            if numerator > 0 {
+                
+                portion += wholePortions > 0 ? " " : ""
+                portion += "\(numerator)/\(denominator)"
+                
+            }
+            
+        }
+        if var unit = ingredient.unit {
+            
+            var suffix = ""
+            
+            if wholePortions > 1 {
+                if unit.suffix(2) == "ch" {
+                    unit += "es"
+                }
+                else if unit.suffix(1) == "f"{
+                    unit = String(unit.dropLast())
+                    unit += "ves"
+                }else{
+                    unit += "s"
+                }
+            }
+            
+            
+            portion += ingredient.num == nil && ingredient.denom == nil ? "" : " "
+            
+            return portion + unit
+        }
         
-        //reduce fraction by greatest common divisor
-        //get the whole portion if numerator > denominator
-        return String(targetServings)
+        return portion
     }
     
 }
